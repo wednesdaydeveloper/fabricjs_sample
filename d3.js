c04d40f044e6efbec3c727486e08bcb676d062b4
wednesday_developer <wednesday.developer@gmail.com>
2016-10-31 00:26:00 +0000

    d3で画像が表示できた

---
+++ d3.js
@@ -0,0 +1,69 @@
+window.onload = function()
+{
+        var imageAnimation = new function(){
+        // 複数画像対応.
+    	var instances = {};
+        this.init = function(id,baseId){ instances[id] = new ImageAnimation(baseId); };    
+    
+        function ImageAnimation(id){
+            var drag,dgrop;
+            
+            //ドラッグ時のイベントをセット
+            drag = d3.behavior.drag()
+            .on("drag", function(d,i) {
+                d.x += d3.event.dx;
+                d.y += d3.event.dy;
+                d3.select(this).attr("transform", function(d,i){
+                    return "translate(" + [ d.x,d.y ] + "),rotate("+d.r+",160, 160),scale("+d.scale+","+d.scale+")";
+                }) 
+            });    
+
+         dgrop = d3.select(id).append("g")
+         .data([ {"x":20, "y":20, "r":1 , "scale":1} ])
+         .attr("x",0)
+         .attr("y",0)
+         .attr("transform","translate(0,0)")
+         .append('image')
+         .attr("x",0)
+         .attr("y",0)
+         .attr("width",300)
+         .attr("height",300)
+         .attr("xlink:href","googlelogo_color_272x92dp.png")
+         .call(drag);   
+
+    
+            // //回転
+            // $("#wheel").bind("click",function(){
+            //     dgrop.attr("transform", function(d,i){
+            //         d.r=d.r-30;
+            //         return "translate(" + [ d.x,d.y ] + "),rotate("+d.r+" ,160, 160),scale("+d.scale+","+d.scale+")";
+            //     }); 
+            // });
+        
+            // //拡大
+            // $("#big").bind("click",function(){
+            //     dgrop.attr("transform", function(d,i){
+            //         d.scale=d.scale*1.2;
+            //         return "translate(" + [ d.x,d.y ] + "),rotate("+d.r+" 160 160),scale("+d.scale+","+d.scale+")";
+            //     });
+            // });
+            
+            // //縮小
+            // $("#small").bind("click",function(){
+            //     dgrop.attr("transform", function(d,i){
+            //         d.scale=d.scale*0.8;
+            //         return "translate(" + [ d.x,d.y ] + "),rotate("+d.r+" 160 160),scale("+d.scale+","+d.scale+")";
+            //     });
+            // });  
+        }
+    }
+    
+    //追加
+    imageAnimation.init(0,"#animation");
+    
+    // var imagenum = 1;
+    // $("#addImage").bind("click",function(d){
+    //     imageAnimation.init(imagenum,"#animation");
+    //     imagenum++;
+    // });  
+};
